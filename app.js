bf8b8ffc347b63ea9b41ff9449ab601004521135
System Administrator <root@Joshs-MacBook-Air.local>
2023-11-09 20:48:16 -0600

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello, I am Joshua Palli'));
+app.get('/', (req, res) => res.send('Full Pipeline Test. If you see this message, the pipline test was a success!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
